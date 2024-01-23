fd2be340d6f03860b74513b8a49d2b22ff611dc6
Phonktezy <144049246+Phonktezy@users.noreply.github.com>
2024-01-23 11:29:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,72 @@
+//1 let num = 3; 
+// alert(num); 
+ 
+//2 let a = 10; 
+// let b = 2; 
+ 
+// console.log(a + b); 
+// console.log(a - b); 
+// console.log(a * b); 
+// console.log(a / b); 
+ 
+//3 let c = 15; 
+// let d = 2; 
+ 
+// result = d + c; 
+// console.log(result); 
+ 
+//4 let a = 10; 
+// let b = 2; 
+// let c = 5; 
+ 
+// alert(a + b + c); 
+ 
+//5 let a = 17; 
+// let b = 10; 
+ 
+// let c = a - b; 
+// let d = c; 
+ 
+// console.log(c); 
+ 
+//6 let str = 'Hello world'; 
+// alert(str) 
+ 
+//7 let str1 = 'Hi, '; 
+// let str2 = 'World!'; 
+ 
+// alert(str1 + str2); 
+ 
+//8 let name = 'Yougene'; 
+// console.log(`hello ${name}`); 
+ 
+//9 let age = 17; 
+// console.log(`you ${age} years old!`); 
+ 
+//11 let name = prompt('whats your name?', ); 
+// alert(`Your name ${name}`); 
+ 
+//12 let x = prompt('Enter num', ); 
+// alert(x * x); 
+ 
+//13 let str = 'abcde'; 
+// console.log(str[0] + str[2] + str[4]) 
+ 
+//14 let num = '12345'; 
+// console.log(num[0] * num[1] * num[2] * num[3] * num[4]); 
+ 
+//15 let x = 1; // hour 
+// let y = 24; // day
+// let z = 720; // month
+ 
+// console.log(`in one hour ${x * 60 * 60} seconds, while day has ${y * 60 * 60} seconds in one month we have ${z * 60 * 60} seconds`) 
+ 
+//16 let x = 10 // hour
+// let y = 57 // minutes 
+// let z = 12 // seconds 
+ 
+// console.log(`${x}:${y}:${z}`); 
+ 
+//17 let x = 12; 
+// c = x * x 
+// console.log(c)
